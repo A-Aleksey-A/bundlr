9e4bc93e8d65626910e84ffd33743e1bd9711888
A-Aleksey-A <126988346+A-Aleksey-A@users.noreply.github.com>
2023-05-06 16:52:32 +0300

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+
+# Define an array of server IP addresses
+servers=(
+  "194.146.12.107"
+  "38.242.222.82"
+  "194.146.12.105"
+  # Add more IP addresses as needed
+)
+
+# Loop over the server IP addresses
+for ip in "${servers[@]}"
+do
+  # Define the destination path
+  dest="C:/Users/akaly/Desktop/test_results/Server-${ip##*.}/test-file.txt"
+
+  # Copy the file from the remote server to the local machine
+  scp -r "root@$ip:/root/backup_test/test-file.txt" "$dest"
+done
